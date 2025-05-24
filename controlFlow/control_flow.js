c7b65bd36fb00c3e514013ee02dab5eeae70081f
jesusmacias0320 <jesusmacias0320@gmail.com>
2025-05-23 23:59:24 -0400

    message

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -51,3 +51,23 @@ let isAuthenticated = true;
 let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 
 console.log("Authentication Status:", authenticationStatus);
+
+
+let roleServices ="empleado";
+let authoricedLevel;
+
+if (roleServices === "empleado"){
+    authoricedLevel = "Empleado!, Acceso autorizado a los Servicios Dietéticos";
+}else if (roleServices === "miembro inscrito" ){
+    authoricedLevel = "Miembro Inscrito!, Acceso autorizado a los Servicios Dietéticos y a una interacción uno a uno con un dietista. ";
+}else if (roleServices === "suscriptor" ){
+    authoricedLevel = "Suscriptor!, Acesso autorizado parcial para facilitar unicamnete los 'Servicios Dietéticos' ";
+
+}else if(roleServices === "no suscriptor"){
+    authoricedLevel = " No Suscriptor!, Necesistas realizar la suscripcion para acceder a esta instalacion";
+    
+}else{
+    authoricedLevel = "Desconosco tu soliticud";
+}
+
+console.log("Role Service: ", authoricedLevel);
